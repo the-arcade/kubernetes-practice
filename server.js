77d9c998b4f01f8bfc73ea9d08fc2bf1d3a349af
Dillon Jeffers <dillon.jeffers@gmail.com>
2017-03-24 21:09:20 -0400

    add an environment variable for variable routes

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express"),
       PORT = 8080,
-      app = express();
+      app = express(),
+      ROUTE = process.env.ROUTE || "/";
 
-app.get("/", function (req, res) {
+app.get(ROUTE, function (req, res) {
     res.send("Hello, world!\n");
 });
 
 app.listen(PORT);
-console.log("Running on http://localhost:", PORT);
+console.log(`route ${ROUTE} running on http://localhost:`, PORT);
