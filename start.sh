e8999165b861acb0c8c79f90cdc9a637ff2b7574
Dillon Jeffers <dillon.jeffers@gmail.com>
2017-03-25 11:55:18 -0400

    start script

---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# build image
+docker build -t djeffer/cluster-api .
+
+# create or replace deployment
+kubectl get deployments | grep cluster-api > /dev/null 2>&1
+STATUS=$?
+if [ $STATUS -eq 0 ]
+    then kubectl replace -f deployment.yml
+    else kubectl create -f deployment.yml
+fi
+
+# create service
+kubectl get services | grep cluster-api > /dev/null 2>&1
+STATUS=$?
+if [ $STATUS -eq 1 ]
+    then kubectl expose deployment cluster-api --type=LoadBalancer
+fi
+
+# view service in browser
+minikube service cluster-api
